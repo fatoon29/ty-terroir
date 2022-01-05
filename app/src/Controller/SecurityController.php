bd256f6a83902b3fcb9045a0b928226f4580a2a6
fatoon29 <fab.perrichon@gmail.com>
2022-01-05 14:46:29 +0100

    feat: add profile page

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -33,4 +33,12 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @Route("/profil", name="user_profil")
+     */
+    public function profil()
+    {
+        return $this->render('security/profil.html.twig');
+    }
 }
